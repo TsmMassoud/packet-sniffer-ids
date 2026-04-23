7369f870d4d4e59ae3cf91aa9b0515a023dc6597
TsmMassoud <ts.massoud@gmail.com>
2026-04-23 17:16:26 +0200

    Init of capture.c

--- include/capture.h
+++ include/capture.h
@@ -1,1 +1,15 @@
+#ifndef CAPTURE_H
+#define CAPTURE_H
 
+#include <pcap/pcap.h>
+
+/* Buffer error size */
+#define PCAP_ERRBUF_SIZE 256
+
+/* Callback function called for every snap received */
+void callback_analyse(u_char *user, const struct pcap_pkthdr *header, const u_char *packet);
+
+/* Main function for capture */
+void start_capture(void);
+
+#endif 
--- src/capture.c
+++ src/capture.c
@@ -1,1 +1,44 @@
+#include <stdio.h>
+#include <pcap/pcap.h>
 
+/* Callback function which sends snaps to parser lately */
+
+void callback_analyse(u_char *user, const struct pcap_pkthdr *header, const u_char *packet){
+    printf("Packet received. Length : %u\n",(unsigned int)header->len);
+}
+
+
+/* Function : capture snap and call a callback function for every snap received */
+void start_capture(){
+    
+    /******************************/
+    /******* INITIALISATION *******/
+    /******************************/
+
+    char *device = "eth0";
+    char errbuf[PCAP_ERRBUF_SIZE];
+    pcap_t *handle = pcap_open_live(device,65535,1,1000,errbuf);
+
+    if(handle == NULL){
+        fprintf(stderr,"Error while opening device %s: %s\n",device,errbuf);
+        return;
+    }
+
+    /* -------------------------- */
+
+    /******************************/
+    /********* LISTENING **********/
+    /******************************/
+
+    printf("Listening on interface %s...",device);
+
+    /* Capture */
+    pcap_loop(handle,-1,callback_analyse,NULL);
+
+    /* -------------------------- */
+
+    /* Close pcap */
+    pcap_close(handle);
+
+
+}
