d85a8a9287ab7a81c2f225b10ef4ea96348dfb02
TsmMassoud <ts.massoud@gmail.com>
2026-04-26 15:32:32 +0200

    parser + makefile

--- Makefile
+++ Makefile
@@ -1,1 +1,31 @@
+CC = gcc
+CFLAGS = -Wall -g
+INCLUDES = -Iinclude
+LIBS = -lpcap
 
+# File list
+SRCS = $(wildcard src/*.c)
+OBJS = $(SRCS:src/%.c=obj/%.o)
+TARGET = bin/my_prog
+
+# Default rule
+all: $(TARGET)
+
+# Linking rule to create the executable
+$(TARGET): $(OBJS)
+	@mkdir -p bin
+	@echo "Linking binary : $@"
+	@$(CC) $(OBJS) -o $(TARGET) $(LIBS)
+
+# Compilation rule for object files
+obj/%.o: src/%.c
+	@mkdir -p obj
+	@echo "File compiled : $<"
+	@$(CC) $(CFLAGS) $(INCLUDES) -c $< -o $@
+
+# Clean up generated files
+clean:
+	@echo "Cleaning..."
+	@rm -rf obj bin
+
+.PHONY: all clean
--- src/parser.c
+++ src/parser.c
@@ -1,1 +1,26 @@
+#include <netinet/ip.h> // For struct ip
+#include <netinet/in.h> // For addr struct (like sockaddr_in)
+#include <pcap/pcap.h>
+#include <arpa/inet.h> // For conversion like inet_ntoa
+#include <stdio.h>
+#include <pcap/pcap.h>
+
+void parse_packet(const u_char *packet,const struct pcap_pkthdr *header){
+
+    // + 14 TO IP
+    const u_char *new_packet = packet + 14;
+    struct ip *ip_header = (struct ip*)new_packet; // to struct ip
+
+
+    // Extract IP_src and IP_dest
+    char *ip_src,*ip_dest;
+    ip_src = inet_ntoa(ip_header->ip_src);
+    ip_dest = inet_ntoa(ip_header->ip_dst);
+
+
+    // Print Ip addr
+    printf("IP source : %s\n",ip_src);
+    printf("IP destination : %s\n",ip_dest);
+
+}
 
